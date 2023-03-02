4ea2e5ac0aa74415575de72b6b61a1d6e24411a6
felipetodev <fe.ossandon.u@gmail.com>
2023-03-02 00:04:21 -0300

    feat: add getCode inline suggestion mode

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,40 @@ import * as vscode from 'vscode';
 import { Panel } from './Panel';
 import { OpenAIStream } from './OpenAI';
 import { loaderMessage } from './utils/loaderMessage';
+import { languageSupportsComments, parseLineComment } from './consts/comments';
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand('cohere-vscode.helloWorld', () => {
+    vscode.commands.registerCommand('colabot-vscode.getCode', async () => {
       const editor = vscode.window.activeTextEditor;
-      const selection = editor?.selection;
-      const selectedText = editor?.document.getText(selection);
-      vscode.window.showInformationMessage(`${selectedText}`);
+      if (!editor) {
+        return vscode.window.showErrorMessage("Select code");
+      }
+      const lang = editor.document.languageId;
+      const isLanguageSupported = languageSupportsComments(lang);
+
+      if (!isLanguageSupported) {
+        return vscode.window.showErrorMessage("Language not supported");
+      }
+      
+      const position = editor.selection.active;
+      const currentLine = editor.document.lineAt(position);
+      
+      const comment = `${parseLineComment(currentLine.text, lang)} for ${lang}`;
+    
+      try {
+        loaderMessage("Please wait...");
+        const { data } = await OpenAIStream(comment!);
+        Panel.createOrShow(context.extensionUri, data.choices[0].text!);
+      } catch (err) {
+        vscode.window.showErrorMessage((err as Error).message);
+      }
     })
   );
 
+  /**
+   * WIP: another use cases
+   */
   context.subscriptions.push(
     vscode.commands.registerCommand('cohere-vscode.doSomething', async () => {
       const answer = await vscode.window.showInformationMessage(
