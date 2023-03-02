5c9010e770a4f4dd36642a80f21f581664dcdf63
felipetodev <fe.ossandon.u@gmail.com>
2023-03-02 00:08:25 -0300

    refact exec commands and add keybinding

--- src/extension.ts
+++ src/extension.ts
@@ -63,23 +63,21 @@ export function activate(context: vscode.ExtensionContext) {
     })
   );
 
-  // ==============================================================================
-
   context.subscriptions.push(
-    vscode.commands.registerCommand('cohere-vscode.helloWebviews', async () => {
+    vscode.commands.registerCommand('colabot-vscode.explainCode', async () => {
       const editor = vscode.window.activeTextEditor;
       const selection = editor?.selection;
       const selectedText = editor?.document?.getText(selection);
       if (!selectedText) {
-        return vscode.window.showInformationMessage('Please select some text first');
+        return vscode.window.showWarningMessage('Please select some text first');
       }
 
       try {
         loaderMessage("Please wait...");
-        const { data } = await OpenAIStream(selectedText);
-        Panel.createOrShow(context.extensionUri, data.choices[0].text || 'No answer found');
+        const { data } = await OpenAIStream(`${selectedText}. Explain how this code works:`);
+        Panel.createOrShow(context.extensionUri, data.choices[0].text!);
       } catch (err) {
-        vscode.window.showInformationMessage((err as Error).message);
+        vscode.window.showErrorMessage((err as Error).message);
       }
     })
   );
