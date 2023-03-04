ae6dde8f0c283fcd5954a85774cd87087d1a936b
felipetodev <fe.ossandon.u@gmail.com>
2023-03-04 00:13:46 -0300

    add secret storage settings

---
+++ src/apiKeySettings.ts
@@ -0,0 +1,34 @@
+import type { ExtensionContext, SecretStorage } from 'vscode';
+
+export default class ApiKeySettings {
+  private static _instance: ApiKeySettings;
+
+  constructor(private secretStorage: SecretStorage) {}
+
+  static init(context: ExtensionContext): void {
+    // Create instance of new ApiKeySettings.
+    ApiKeySettings._instance = new ApiKeySettings(context.secrets);
+  }
+
+  static get instance(): ApiKeySettings {
+    // Getter of our ApiKeySettings existing instance.
+    return ApiKeySettings._instance;
+  }
+
+  async storeKeyData(token?: string): Promise<void> {
+    // Update values in secret storage.
+    if (token) {
+      this.secretStorage.store('colabot_key', token);
+    }
+  }
+
+  async getKeyData(): Promise<string | undefined> {
+    // Retrieve data from secret storage.
+    return await this.secretStorage.get('colabot_key');
+  }
+
+  async deleteKeyData(): Promise<void> {
+    // Delete data from secret storage.
+    await this.secretStorage.delete('colabot_key');
+  }
+}
