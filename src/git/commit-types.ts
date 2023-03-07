d74f994c50088a462453622739425ca4e55ba2e1
felipetodev <fe.ossandon.u@gmail.com>
2023-03-06 22:09:18 -0300

    add semantic commit types opt

---
+++ src/git/commit-types.ts
@@ -0,0 +1,57 @@
+export const COMMIT_TYPES = {
+  ia: {
+    emoji: '🤖',
+    description: 'Submit an IA commit message (ColaBOT)',
+    release: false,
+  },
+  feat: {
+    emoji: '🆕',
+    description: 'Add new feature',
+    release: true, // This will trigger a new release
+  },
+  fix: {
+    emoji: '🐛',
+    description: 'Submit a fix to a bug',
+    release: true,
+  },
+  perf: {
+    emoji: '⚡️',
+    description: 'Improve performance',
+    release: true,
+  },
+  refactor: {
+    emoji: '🛠 ',
+    description: 'Refactor code',
+    release: true,
+  },
+  docs: {
+    emoji: '📚',
+    description: 'Add or update documentation',
+    release: false,
+  },
+  test: {
+    emoji: '🧪',
+    description: 'Add or update tests',
+    release: false,
+  },
+  build: {
+    emoji: '🏗️ ',
+    description: 'Add or update build scripts',
+    release: false,
+  },
+  ci: {
+    emoji: '🚦',
+    description: 'Add or update CI/CD scripts',
+    release: false,
+  },
+  chore: {
+    emoji: '🧹',
+    description: 'Other changes that don\'t modify src or test files',
+    release: false,
+  },
+  revert: {
+    emoji: '⏪',
+    description: 'Revert to a commit',
+    release: false,
+  },
+};
