37337069efcc29934fed86f7102634a2eb66a1d0
felipetodev <fe.ossandon.u@gmail.com>
2023-03-18 01:53:38 -0300

    refact to include a sidebar component and handle openai call

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,41 +1,62 @@
-import { useState } from "react";
-import { vscode } from "./utils/vscode";
-import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
-import Layout from "./components/Layout";
-import Clipboard from "./components/Clipboard";
+import { useState } from 'react';
+import { vscode } from './utils/vscode';
+import { VSCodeButton } from '@vscode/webview-ui-toolkit/react';
+import Layout from './components/Layout';
+import Clipboard from './components/Clipboard';
+import Sidebar from './components/Sidebar';
+import type { OpenAIStreamPayload } from './types';
 
 declare global {
   interface Window {
     responseText?: string;
+    sidebar?: boolean;
+    openAIPayload?: OpenAIStreamPayload
   }
+}
+
+const ExplaiWebview = ({ response }: { response: string }) => {
+  const handleCloseWebviewPanel = () => {
+    vscode.postMessage({
+      command: 'closeWebviewPanel',
+    });
+  }
+  return (
+    <>
+      <h1 className='text-blue-400 text-3xl font-bold'>
+        Explain Code:
+      </h1>
+      <div className='py-5 overflow-hidden whitespace-pre-wrap overflow-ellipsis break-words'>
+        {response || 'No response from AI provider'}
+      </div>
+      <VSCodeButton className='w-full' onClick={handleCloseWebviewPanel}>
+        Close
+      </VSCodeButton>
+      <Clipboard content={response} />
+    </>
+  )
 }
 
 function App() {
   const [aiResponse] = useState<string | undefined>(() => {
     if (window !== undefined) {
-      return window.responseText;
+      return window?.responseText;
+    }
+  })
+  const [sidebar] = useState<boolean | undefined>(() => {
+    if (window !== undefined) {
+      return window?.sidebar || false;
     }
   })
 
-  const handleCloseWebviewPanel = () => {
-    vscode.postMessage({
-      command: "closeWebviewPanel",
-    });
-  }
-
   return (
-    <Layout>
-      <h1 className="text-blue-400 text-3xl font-bold">
-        Explain Code:
-      </h1>
-      <div className="py-5 overflow-hidden whitespace-pre-wrap overflow-ellipsis break-words">
-        {aiResponse || "No response from AI provider"}
-      </div>
-      <VSCodeButton className="w-full" onClick={handleCloseWebviewPanel}>
-        Close
-      </VSCodeButton>
-      <Clipboard content={aiResponse} />
-    </Layout>
+    <>
+      {aiResponse && (
+        <Layout>
+          <ExplaiWebview response={aiResponse} />
+        </Layout>
+      )}
+      {sidebar && <Sidebar />}
+    </>
   );
 }
 
--- webview-ui/src/components/Layout.tsx
+++ webview-ui/src/components/Layout.tsx
@@ -1,5 +1,5 @@
 
-export default function Layout({ children }: {children: React.ReactNode}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="relative mt-5 py-5 px-4 bg-[#18181a] text-white rounded-lg">
       {children}
