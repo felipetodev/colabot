5db397d39c80823d15c5324314c0f9fc1c48a6fd
felipetodev <fe.ossandon.u@gmail.com>
2023-03-20 00:09:12 -0300

    handle api errors

--- webview-ui/src/utils/openai.ts
+++ webview-ui/src/utils/openai.ts
@@ -1,9 +1,3 @@
-// import {
-//   createParser,
-//   ParsedEvent,
-//   ReconnectInterval,
-// } from "eventsource-parser";
-
 import type { ChatState, OpenAIStreamPayload } from "../types";
 
 export async function OpenAIStream (chatState: ChatState, vsCodePayload: OpenAIStreamPayload) {
@@ -18,19 +12,29 @@ export async function OpenAIStream (chatState: ChatState, vsCodePayload: OpenAIS
   const { apiKey, ...payload } = vsCodePayload;
 
   if (payload.model === "gpt-3.5-turbo") {
-    const res = await fetch("https://api.openai.com/v1/chat/completions", {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`,
-      },
-      method: "POST",
-      body: JSON.stringify({
-        messages: chatState,
-        ...payload,
-      }),
-    });
-    const completion = await res.json();
-    return completion.choices[0].message?.content
+    try {
+      const res = await fetch("https://api.openai.com/v1/chat/completions", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          messages: chatState,
+          ...payload,
+        }),
+      });
+      const completion = await res.json();
+
+      if (completion.error) {
+        return completion.error.message
+      }
+
+      return completion.choices[0].message?.content
+    } catch(err) {
+      console.error(err)
+      return "I'm sorry, I'm having trouble connecting to the OpenAI API. Please try again later."
+    }
   }
 
   let prompt = '"[user]:" means that some user is asking you for help\n\n'
@@ -41,53 +45,23 @@ export async function OpenAIStream (chatState: ChatState, vsCodePayload: OpenAIS
       : prompt += `${[role]}: ${content}\n`
   }
 
-  const res = await fetch("https://api.openai.com/v1/completions", {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`,
-    },
-    method: "POST",
-    body: JSON.stringify({
-      prompt,
-      ...payload,
-    }),
-  });
-
-  const completion = await res.json();
-
-  /*
-    const stream = new ReadableStream({
-      async start(controller) {
-        function onParse(event: ParsedEvent | ReconnectInterval) {
-          if (event.type === "event") {
-            const data = event.data;
-            if (data === "[DONE]") {
-              controller.close();
-              return;
-            }
-            try {
-              const json = JSON.parse(data);
-              const text = json.choices[0].text;
-              if (counter < 2 && (text.match(/\n/) || []).length) {
-                // this is a prefix character (i.e., "\n\n"), do nothing
-                return;
-              }
-              const queue = encoder.encode(text);
-              controller.enqueue(queue);
-              counter++;
-            } catch (e) {
-              // maybe parse error
-              controller.error(e);
-            }
-          }
-        }
-        const parser = createParser(onParse);
-        for await (const chunk of res.body as any) {
-          parser.feed(decoder.decode(chunk));
-        }
+  try {
+    const res = await fetch("https://api.openai.com/v1/completions", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
       },
+      method: "POST",
+      body: JSON.stringify({
+        prompt,
+        ...payload,
+      }),
     });
-  */
-
-  return completion.choices[0].text;
+  
+    const completion = await res.json();
+    return completion.choices[0].text;
+  } catch (err) {
+    console.error(err)
+    return "I'm sorry, I'm having trouble connecting to the OpenAI API. Please try again later."
+  }
 }
